16e947b86ba14ce2ca3829827331f4f484e8ac26
Arankin7 <30415670+Arankin7@users.noreply.github.com>
2022-11-08 15:10:04 -0500

    added dropdown, searching for Trending and UserInput works, still need to append it to the page

--- news-api/src/App.js
+++ news-api/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,10 +16,23 @@ function App() {
 
       <header>
         <Header></Header>
+        
       </header>
       
       <main>
+      <Dropdown>
+        <Dropdown.Toggle variant="dark" id="dropdown-basic">
+          Sort
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item>Alphabetically</Dropdown.Item>
+          <Dropdown.Item>Publication Date</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
         <TopStories></TopStories>
+        
 
         <ArticleSearch></ArticleSearch>
       </main>
--- news-api/src/components/ArticleSearch/index.js
+++ news-api/src/components/ArticleSearch/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 
 function ArticleSearch() {
 
+    const [userInput, setUserInput] = useState('');
+
+    const handleChange = event => {
+        setUserInput(event.target.value);
+
+        console.log('searching for: ', event.target.value);
+    }
+
+    // const userInput = document.getElementById("userInput").value;
+    const apiUrl = 'https://newsapi.org/v2/everything?q='+ userInput + '&apiKey=2b7c3079f57b4775b9829a555f91c54d';
+
+    function UserSearch(userInput){
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+                console.log('articles searched for: ', data);
+            });
+    }
+
     return (
         <div>
             <h1>Search for Articles</h1>
-            <input></input>
-            <Button variant="primary">Search</Button>
+            <input type="text" placeholder="Yummy Tacos" value={userInput} onChange={handleChange}></input>
+            <Button variant="primary" onClick={UserSearch}>Search</Button>
         </div>
     )
 }
